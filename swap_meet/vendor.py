861da2f2a85bf0ea080c1144cabc05ea7216347e
Hannah <hannahtan96@gmail.com>
2022-10-03 11:44:08 -0700

    created vendor class and passed all wave1 tests

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,2 +1,15 @@
 class Vendor:
-    pass
+    
+    def __init__(self, inventory=[]):
+        self.inventory = inventory
+
+    def add(self, item):
+        self.inventory.append(item)
+        return item
+
+    def remove(self, item):
+        try:
+            self.inventory.remove(item)
+            return item
+        except:
+            return False
