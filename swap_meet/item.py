2f2d9c569f3629a43a36a1eb615fb119c8b10409
Hannah <hannahtan96@gmail.com>
2022-10-03 13:53:10 -0700

    added item class and passed all unit and integration tests for wave2 and wave3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,7 @@
 class Item:
-    pass
+    
+    def __init__(self, category=''):
+        self.category = category
+
+    def __str__(self):
+        return "Hello World!"
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -1,7 +1,13 @@
+from re import T
+from tkinter.tix import Tree
+from swap_meet.item import Item
+
 class Vendor:
     
-    def __init__(self, inventory=[]):
+    def __init__(self, inventory=None):
         self.inventory = inventory
+        if self.inventory is None:
+            self.inventory = []
 
     def add(self, item):
         self.inventory.append(item)
@@ -12,4 +18,15 @@ class Vendor:
             self.inventory.remove(item)
             return item
         except:
+            return False
+
+    def get_by_category(self, category):
+        return [item for item in self.inventory if item.category == category]
+
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.add(other_vendor.remove(their_item))
+            other_vendor.add(self.remove(my_item))
+            return True
+        else: 
             return False
